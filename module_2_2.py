e854015f0615cdd59581fe943f16aa8570a5abf2
teestar5 <147434843+teestar5@users.noreply.github.com>
2024-11-16 00:01:30 +0300

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -1,27 +1,18 @@
-#Если все числа равны между собой, то вывести 3
-first = 25
-second = 25
-third = 25
 
-if first == second == third:
+
+print('Введите поочерёдно три целых числа')
+first = int(input('Введите первое число: '))
+second = int(input('Введите второе число: '))
+third = int(input('Введите третье число: '))
+
+print('Количество одинаковых чисел')
+
+if first == second and first == third:
     print(3)
-
-#Если хотя бы 2 из 3 введённых чисел равны между собой, то вывести 2
-first = 25
-second = 30
-third = 25
-
-if first == second or first == third or second == third:
+elif first == second or first == third or second == third:
     print(2)
-
-#Если равных чисел среди 3-х вообще нет, то вывести 0
-first = 25
-second = 30
-third = 25
-if first != second or first!= third or second != third:
+else:
     print(0)
-
-
 
 
 
